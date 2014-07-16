4b149f9cc633848543817410d2840928ad352940
kdbanman <kdbanman@ualberta.ca>
2014-07-16 15:50:07 -0600

    scaffold field generator architecture

--- public/rand-gen.js
+++ public/rand-gen.js
@@ -27,6 +27,7 @@ rGen.generate = function* (fieldCount, seed)
 
         // use the generated field name and assign it a field value
         for (let field of rGen.generateField(fieldCount, rng)) {
+            console.log("debug");
             obj[fieldName] = field;
             yield obj;
         }
@@ -39,22 +40,95 @@ rGen.generateFieldName = function* (rng)
     yield "butt";
 }
 
-rGen.generateField = function* (length, rng)
+rGen.generateField = function (length, rng)
 {
-    //TODO
-    yield 133780085;
-}
+    var generator = [
+        function () {
+            // integer generator
+            return rGen.generateInt(0xfffff, rng);
+        },
 
-rGen.generateInt = function* (rightBound, rng, illegal)
-{
-    //TODO
-    yield 42;
+        function () {
+            // double generator
+            return (function* () {
+                yield 0xfffff * (rng.random() - 1);
+            })();
+        },
+
+        function () {
+            // boolean generator
+            return (function* () {
+                yield rng.random() < 0.5 ? true : false;
+            })();
+        },
+
+        function () {
+            // string generator
+            return rGen.generateString(length, rng);
+        },
+
+        function () {
+            // null generator
+            return (function* () {
+                yield null;
+            })();
+        },
+
+        function () {
+            // undefined generator
+            return (function* () {
+                yield undefined;
+            })();
+        },
+
+        function () {
+            // Infinity generator
+            return (function* () {
+                yield 1/0;
+            })();
+        },
+
+        function () {
+            // NaN generator
+            return (function* () {
+                yield Math.sqrt(-1);
+            })();
+        },
+
+        function () {
+            // object generator
+            return rGen.generateObject(length, rng);
+        },
+
+        function () {
+            // array generator
+            return rGen.generateArray(length, rng);
+        }
+    ];
+
+    // return object field generator
+    var i = rGen.generateInt(generator.length, rng).next().value;
+    return generator[i]();
 }
 
 rGen.generateString = function* (length, rng)
 {
     //TODO
     yield "not a string";
+}
+
+rGen.generateInt = function* (rightBound, rng, illegal)
+{
+    // nothing illegal by default
+    var illegal = illegal || function () {return false;};
+
+    var rand = function () {
+        var candidate = Math.floor(rng.random() * rightBound);
+        // recursively call until non-illegal candidate found
+        return illegal(candidate) ? rand() : candidate;
+    }
+
+    yield rand();
 }
 
 rGen.generateObject = function* (length, rng)
@@ -74,7 +148,7 @@ rGen.generateArray = function* (length, rng)
     }
 }
 
-var gen = rGen.generate(10, 42);
+var gen = rGen.generate(10, 43);
 var timer = setInterval(function () {
     var next = gen.next();
     if (next.done) clearInterval(timer);
